f808d318e72a550872a422eaf527111ee53643cb
Ali Nazari <ali.nazari@aut.ac.ir>
2019-08-22 15:31:28 +0430

    refactor sherbox.js

--- src/components/SherBox.js
+++ src/components/SherBox.js
@@ -3,10 +3,9 @@ import "./styles/sher-style.css"
 
 class SherBox extends React.Component { 
     render() { 
-        let src = "./../img/trophy.png";
         return(
             <div className = "sher-container">
-                <img src= {this.props.url} role="presentation" width ="160" height="160" />
+                <img src={this.props.url} width ="160" height="160" />
                 <h1>{this.props.title}</h1>
                 <p>{this.props.text}</p>
             </div>
