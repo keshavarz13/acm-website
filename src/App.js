5fc0126d5bcb22222a466fd9117642ee8daa47a5
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-22 09:14:11 +0330

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react" 
+import Header from "./components/Header"
+
+class App extends React.Component { 
+    render(){
+        return(
+            <Header/>
+        )
+    }
+}
+
+export default App
