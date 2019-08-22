e308c887cf97b218fe52547ad0bb6236722c3006
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-22 09:24:43 +0330

    add links and acm logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ class Header extends React.Component {
     render(){
       return(
         <div>
-
+            <div className = "container">
+                <a href="#">Home</a>
+                <a href="#" >Post Result</a>
+                <a href="#">About</a>
+                <img src="http://icpc.aut.ac.ir/img/aut_icpc.png" />
+            </div>
         </div>
       )
     }
