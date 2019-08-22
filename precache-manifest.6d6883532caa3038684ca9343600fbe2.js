74f060a320af1057e84ff8826cc52421eea43e20
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-22 16:33:10 +0330

    Updates

---
+++ precache-manifest.6d6883532caa3038684ca9343600fbe2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6555aac1c1703b659e74f641b56c889a",
+    "url": "/acm-website/index.html"
+  },
+  {
+    "revision": "7fcea798c903a7a28645",
+    "url": "/acm-website/static/css/main.5bf5d885.chunk.css"
+  },
+  {
+    "revision": "c8fb423c0cd77018198f",
+    "url": "/acm-website/static/js/2.6be2bf10.chunk.js"
+  },
+  {
+    "revision": "7fcea798c903a7a28645",
+    "url": "/acm-website/static/js/main.0f55f7eb.chunk.js"
+  },
+  {
+    "revision": "c1427d4c31547c8627bc",
+    "url": "/acm-website/static/js/runtime~main.bb3bbd2d.js"
+  },
+  {
+    "revision": "8863582d824d649d0d462cfb94a3b4f4",
+    "url": "/acm-website/static/media/Bahij_Palatino_Sans_Arabic-Regular.8863582d.ttf"
+  },
+  {
+    "revision": "57213dceed6a6294e5655091e1d0ad9f",
+    "url": "/acm-website/static/media/aut_icpc2.57213dce.png"
+  },
+  {
+    "revision": "de732a074e7bdd9ccccaa84640d77774",
+    "url": "/acm-website/static/media/banner.de732a07.png"
+  },
+  {
+    "revision": "d33701315c1d6d14223d8ff98a4e8b9c",
+    "url": "/acm-website/static/media/banner2.d3370131.png"
+  },
+  {
+    "revision": "77c70836ae51c286642bfd5c87267360",
+    "url": "/acm-website/static/media/bottomImage.77c70836.JPG"
+  },
+  {
+    "revision": "970700edfcd739aff3d146b210325d0c",
+    "url": "/acm-website/static/media/group.970700ed.png"
+  },
+  {
+    "revision": "3673b79179c1a55461d04d8482dbfa9e",
+    "url": "/acm-website/static/media/gym.3673b791.png"
+  },
+  {
+    "revision": "ffc2a85c0dc0a87f2afa48abf3656bdd",
+    "url": "/acm-website/static/media/ssc-logo.ffc2a85c.png"
+  },
+  {
+    "revision": "5c5b9f1a56b71c95f89d03b019958d49",
+    "url": "/acm-website/static/media/topImage.5c5b9f1a.JPG"
+  },
+  {
+    "revision": "5799083ff73f1fbff9f0ddf9615f5148",
+    "url": "/acm-website/static/media/trophy.5799083f.png"
+  }
+]);
