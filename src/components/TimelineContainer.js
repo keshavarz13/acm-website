4623d1a13b993986db752fd37faf5b806ebdcb5f
Ali Nazari <ali.nazari@aut.ac.ir>
2019-08-22 23:02:46 +0430

    edit timeline

--- src/components/TimelineContainer.js
+++ src/components/TimelineContainer.js
@@ -14,7 +14,6 @@ class TimelineContainer extends React.Component{
                     >
                         <h3>Registration Dates</h3>
                         <p>
-                            
                             The contest will be held both online and on-site.<br></br>
                             <strong>Registration period of On-Site contest for Iranian teams is from 17th to 28th of October 2019</strong>.<br></br>
                             Others can register until 4th of November 2019.
@@ -40,11 +39,11 @@ class TimelineContainer extends React.Component{
                         <h3>On-Site Contest</h3>
                         <p>
                             07:00:00 (UTC)<br></br>
-                            10:30:00 (GMT +3:30 - Tehran, Iran Standard Time)<br></br><br></br>
+                            <strong>10:30:00 (GMT +3:30 - Tehran, Iran Standard Time)</strong><br></br>
                             The online contest will be held at least two hours after the on-site contest.
                         </p>
                     </TimelineItem>
-                    </Timeline>
+                </Timeline>
             </div>
         )
     }
