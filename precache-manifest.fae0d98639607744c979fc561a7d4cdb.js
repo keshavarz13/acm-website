985d281322b559ea20bc5e1472126316fcf91568
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-22 10:17:09 +0330

    Updates

---
+++ precache-manifest.fae0d98639607744c979fc561a7d4cdb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4c97128f589ac93aaf795f12a68393",
+    "url": "/acm-website/index.html"
+  },
+  {
+    "revision": "7cf83c741b02615450ae",
+    "url": "/acm-website/static/css/main.fb4ecab6.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/acm-website/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "7cf83c741b02615450ae",
+    "url": "/acm-website/static/js/main.2ad6b5a7.chunk.js"
+  },
+  {
+    "revision": "c1427d4c31547c8627bc",
+    "url": "/acm-website/static/js/runtime~main.bb3bbd2d.js"
+  },
+  {
+    "revision": "57213dceed6a6294e5655091e1d0ad9f",
+    "url": "/acm-website/static/media/aut_icpc2.57213dce.png"
+  },
+  {
+    "revision": "c032202bfc91484d6c4389900ca23a17",
+    "url": "/acm-website/static/media/banner.c032202b.png"
+  },
+  {
+    "revision": "fcd6b891ad64c1ef143587000ce6adb7",
+    "url": "/acm-website/static/media/banner2.fcd6b891.png"
+  },
+  {
+    "revision": "c55936e0a3b92999d096ae29fa3dbd39",
+    "url": "/acm-website/static/media/middleImage.c55936e0.JPG"
+  },
+  {
+    "revision": "5c5b9f1a56b71c95f89d03b019958d49",
+    "url": "/acm-website/static/media/topImage.5c5b9f1a.JPG"
+  }
+]);
