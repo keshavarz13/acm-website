ecadbe5e8c56bdef34f79d98b1bb5c2544b9ae96
Ali Nazari <ali.nazari@aut.ac.ir>
2019-08-23 17:48:58 +0430

    refactor body 1 and 2

--- src/components/BodyP1.js
+++ src/components/BodyP1.js
@@ -2,26 +2,26 @@ import React from "react"
 import "./styles/body-style.css"
 
 class BodyP1 extends React.Component { 
-    render(){
+    render() {
         return (
             <div>
                 <div className= "container2">
-                    
-                    <img className= "blur-image" src ={require("./../img/topImage.JPG")} width="100%" height = "800px"/>
-                    <div className = "dark-layer"></div>
-                    <div className ="banner-container">
-                        
-                         <img className="banner1" src ={require("./../img/banner.png")}  />
+                    <img 
+                        className="blur-image" 
+                        src ={ require("./../img/topImage.JPG") } 
+                        width="100%" height="800px"
+                    />
+                    <div className= "dark-layer"></div>
+                    <div className="banner-container">
+                        <img className="banner1" src ={require("./../img/banner.png")}  />
                     </div>
-                    <div className ="banner-container">
-                         <img className="banner2" src ={require("./../img/banner2.png")}  />
+                    <div className="banner-container">
+                        <img 
+                            className="banner2" 
+                            src={ require("./../img/banner2.png") }  
+                        />
                     </div>
-                    
                 </div>
-                
-                        
-                
-                
             </div>
         )
     }
--- src/components/BodyP2.js
+++ src/components/BodyP2.js
@@ -4,56 +4,44 @@ import "./styles/body-style.css"
 class BodyP2 extends React.Component { 
     constructor() { 
         super() 
-        this.state = { 
-            d : 0 ,
-            h : 0 , 
-            m : 0 ,
-            s : 0 ,
+        this.state={ 
+            d: 0,
+            h: 0, 
+            m: 0,
+            s: 0,
         }
     }
 
-    render(){
-
- 
-
+    render() {
         return (
             <div>
-                <div className= "container2">
-                    
+                <div className="container2">
+                    <img 
+                        style={{ objectFit: "cover" }} 
+                        className= "blur-image" 
+                        src={ require("./../img/bottomImage.JPG") } 
+                        height="800" width="100%"
+                    />
+                    <div className = "dark-layer"></div>                
 
-                    <img className= "blur-image" src ={require("./../img/bottomImage.JPG")}  height = "800" width="100%"/>
-
-                    <div className = "dark-layer"></div>
-                
-
-                    <p className = "timer-text">Our Next Event Starts in</p>
+                    <p className = "timer-text">Contest starts in</p>
                     <div className ="timer">
-                        <p className  ="timer-number">10</p>
-                        <p className  ="timer-number">10</p>
-                        <p className  ="timer-number">10</p>
-                        <p className  ="timer-number">10</p>
+                        <p className="timer-number">10</p>
+                        <p className="timer-number">10</p>
+                        <p className="timer-number">10</p>
+                        <p className="timer-number">10</p>
                         <p>Days</p>
                         <p>Hours</p>
                         <p>Minutes</p>
                         <p>Seconds</p>
-
                     </div>
-                    <div className ="timer2">
-                        <p className  ="timer-number">10</p><p>Days</p>
-                        <p className  ="timer-number">10</p><p>Hours</p>
-                        <p className  ="timer-number">10</p><p>Minutes</p>
-                        <p className  ="timer-number">10</p><p>Seconds</p>
-                        
-                        
-                        
-                        
-
+                    <div className="timer2">
+                        <p className="timer-number">10</p><p>Days</p>
+                        <p className="timer-number">10</p><p>Hours</p>
+                        <p className="timer-number">10</p><p>Minutes</p>
+                        <p className="timer-number">10</p><p>Seconds</p>
                     </div> 
                 </div>
-                
-                        
-                
-                
             </div>
         )
     }
