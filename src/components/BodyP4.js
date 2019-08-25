bf91b710db0b5b662e0ea1cd84ad4666859d4fad
Ali Nazari <ali.nazari@aut.ac.ir>
2019-08-25 18:33:04 +0430

    change sponsor text

--- src/components/BodyP4.js
+++ src/components/BodyP4.js
@@ -12,7 +12,7 @@ class BodyP4 extends React.Component {
             <div className = "part4-container">
                 <div className= "part4-inner-container">
                     <div className = "sponsor-box">
-                        <h1>SPONSOR</h1>
+                        <h1>EVENT SPONSOR</h1>
                         <div className="line"></div>
                         <div className="sponsor-image"> <img src={require ("./../img/sponsor.png") } width ="250" height="250" /></div>
                     </div>
