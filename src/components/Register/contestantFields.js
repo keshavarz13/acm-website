0bc4ae04a3ccb6d3e14da5c04cf487e4f8a00af8
Ali Nazari <ali.nazari@aut.ac.ir>
2019-08-26 16:16:52 +0430

    add listener to firstname, lastname, gender

--- src/components/Register/contestantFields.js
+++ src/components/Register/contestantFields.js
@@ -6,8 +6,15 @@ class contestantFields extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            gender: null,
-            edu_level: null
+            firstname: "",
+            lastname: "",
+            gender: {
+                gender: ""
+            },         
+            firstname: "",   
+            edu_level: {
+                edu_level: ""
+            }
         }
     }
 
@@ -17,34 +24,37 @@ class contestantFields extends React.Component {
                 <h2>Contestant #{this.props.memberNumber}</h2>
                 <div>
                     <FormControl margin="normal" required >
-                    <InputLabel htmlFor="fistname">First Name</InputLabel>
+                    <InputLabel htmlFor="firstname">First Name</InputLabel>
                     <Input
-                        id="fistname"
                         className="text_box"
-                        // onChange={event => setFirstname(event.target.value)}
+                        onChange={event => this.setState({
+                            firstname: event.target.value})
+                        }
                     />
                     </FormControl>
                     <FormControl margin="normal" required >
                     <InputLabel htmlFor="lastname">Last Name</InputLabel>
                     <Input
-                        id="lastname"
                         className="text_box"
+                        onChange={event => this.setState({
+                            lastname: event.target.value})
+                        }
                     />
                     </FormControl>
-
                     <FormControl margin="normal" required>
                         <InputLabel htmlFor="gender">Gender</InputLabel>
                         <Select 
                             className="text_box"
-                            // value={"ali"}
-                            // onChange={event => this.setState({ 
-                            //     [event.target.name]: event.target.value}) }
-                            // inputProps={{
-                            //     name: "gender",
-                            //     id: "age-simple"
-                            //     }}
+                            value={this.state.gender.gender}
+                            onChange={event => this.setState({ 
+                                gender: {
+                                    gender: event.target.value}
+                                }) 
+                            }
+                            inputProps={{
+                                name: "gender",
+                            }}
                         >
-                        {console.log(this.state.gender)}
                             <MenuItem value={"Male"}>Male</MenuItem>
                             <MenuItem value={"Female"}>Female</MenuItem>
                         </Select>
@@ -55,14 +65,22 @@ class contestantFields extends React.Component {
                     <FormControl margin="normal" required >
                     <InputLabel htmlFor="studentnumber">Student Number</InputLabel>
                     <Input
-                        id="studentnumber"
                         className="text_box"
                     />
                     </FormControl>
-
                     <FormControl margin="normal" required>
                         <InputLabel htmlFor="edu_level">Current Educational Level</InputLabel>
-                        <Select className="text_box">
+                        <Select 
+                            className="text_box"
+                            onChange={event => this.setState({ 
+                                gender: {
+                                    gender: event.target.value}
+                                }) 
+                            }
+                            inputProps={{
+                                name: "gender",
+                            }}
+                        >
                             <MenuItem value={"Undergraduate"}>Undergraduate(BSc.)</MenuItem>
                             <MenuItem value={"Graduate"}>Graduate(MSc.)</MenuItem>
                         </Select>
@@ -71,14 +89,12 @@ class contestantFields extends React.Component {
                     <FormControl margin="normal" required >
                     <InputLabel htmlFor="email">Email</InputLabel>
                     <Input
-                        id="email"
                         className="text_box"
                     />
                     </FormControl>
                     <FormControl margin="normal" required >
                     <InputLabel htmlFor="phonenumber">Phone Number</InputLabel>
                     <Input
-                        id="phonenumber"
                         className="text_box"
                     />
                     </FormControl>
