8b6e43f7d100a254404930411a1d97f78c26f5a3
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-30 12:26:45 +0330

    change functinality of dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,37 @@ import { NavLink } from 'react-router-dom'
 
 
 class Header extends React.Component { 
+    constructor() { 
+      super() 
+      this.state = { 
+          display :1,
+          style : {
+            visibility: "hidden" 
+          }
+      }
+      this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    clickHandler () { 
+       if(this.state.display === 1){
+          this.setState({
+            display : 2 , 
+            style : { 
+              visibility: "visible"
+            }
+          })
+          
+        }
+        else if(this.state.display === 2) { 
+          this.setState({
+            display : 1,
+            style : {
+              visibility: "hidden" 
+            }
+          })
+        }
+    }
+    
     render() {
       return(
         <div>
@@ -20,15 +51,15 @@ class Header extends React.Component {
 
                   <div className ="option">
                     <div class="dropdown">
-                      <button class="dropbtn"><img src = {require("./../img/dropDown.svg")} width ="32" height ="32"/>
+                      <button class="dropbtn"><img src = {require("./../img/dropDown.svg")} width ="32" height ="32" onClick = {this.clickHandler}  />
                         <i class="fa fa-caret-down"></i>
                       </button>
-                      <div class="dropdown-content">
-                          <NavLink onClick={() => window.scrollTo(0, 0)} className="nav-link" exact to="/">Home</NavLink>
-                          <a className="nav-link" href="#">Time Line</a>
-                          <a className="nav-link" href="#">Contact Us</a>
-                          <NavLink onClick={() => window.scrollTo(0, 0)} className="nav-link" exact to="/PastResult">Past Result</NavLink>
-                          <NavLink onClick={() => window.scrollTo(0, 0)} className="nav-link" exact to="/Register">Register</NavLink>
+                      <div class="dropdown-content" style ={this.state.style}>
+                          <NavLink onClick={() => window.scrollTo(0, 0)}onClick = {this.clickHandler} className="nav-link" exact to="/">Home</NavLink>
+                          <a className="nav-link" onClick = {this.clickHandler} href="#">Time Line</a>
+                          <a className="nav-link" onClick = {this.clickHandler} href="#">Contact Us</a>
+                          <NavLink onClick={() => window.scrollTo(0, 0)} onClick = {this.clickHandler} className="nav-link" exact to="/PastResult">Past Result</NavLink>
+                          <NavLink onClick={() => window.scrollTo(0, 0)} onClick = {this.clickHandler} className="nav-link" exact to="/Register">Register</NavLink>
                       </div>
                     </div> 
                   </div>
