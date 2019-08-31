6e855b53eedcf5f7371c67f61b0d0cda078f4573
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-08-31 17:36:34 +0330

    change size of image

--- src/components/PosterList.js
+++ src/components/PosterList.js
@@ -16,73 +16,73 @@ class PosterList extends React.Component {
                         [{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },{
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },
                         {
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         },
                         {
                             src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
                             thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                            thumbnailWidth: 320,
-                            thumbnailHeight: 212,
+                            thumbnailWidth: 400,
+                            thumbnailHeight: 300,
                             tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
                             caption: "Boats (Jeshu John - designerspics.com)"
                         }]
