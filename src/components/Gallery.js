a21533f5b7a7ef215c88439eebc5b22af730cc40
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-09-02 14:56:24 +0330

    make gallery responsive

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,17 +4,30 @@ class Gallery extends React.Component {
     constructor(){
         super()
         this.state = {
+            slideStyles :{
+                display : "none",
+            }
 
         }
     }
     render() {
         return (
             <div>
-                <div className = "slide-show">
+                <div className = "slide-show" style = {this.state.slideStyles}>
                     <img className = "slid-image" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg" />
                     <p>caption</p>
                     <img className = "left" height = "50" width = "50" src = { require("./../img/left.svg") }  />
                     <img className = "right"  height = "50" width = "50"  src = { require("./../img/right.svg") } />
+                </div>
+
+                <div className="gallery-container">
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg" onClick = {() => this.setState({ slideStyles :{display : "block",}})}/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+                     <img className = "photos" src = "https://www.crockerriverside.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
                 </div>
             </div>
         )
