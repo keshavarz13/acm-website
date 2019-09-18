95ef196a3668a0bc22694b9c1e464790df9c5f55
MohammadAli Keshavarz <keshavarz.ma13@gmail.com>
2019-09-19 00:26:20 +0330

    add poster in body part 4

--- src/components/BodyP4.js
+++ src/components/BodyP4.js
@@ -10,9 +10,12 @@ class BodyP4 extends React.Component {
 
         return (
             <div className = "part4-container">
+                 <div className = "new-poster" >
+                        <img src="http://icpc.sharif.edu/2018/images/poster.png" width="300" height="400"/>
+                </div>
                 <div className= "part4-inner-container">
                     <div className = "sponsor-box">
-                        <h1>EVENT SPONSOR</h1>
+                        <h1>Event Sponsor</h1>
                         <div className="line"></div>
                         <div className="sponsor-image"> <img src={require ("./../img/sponsor.png") } width ="250" height="250" /></div>
                     </div>
